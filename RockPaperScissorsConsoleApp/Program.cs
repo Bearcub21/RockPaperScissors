8fe44720772c27b4d8d23a17dcaf5d7b695beedf
William_Gallardo <Bearcub21@users.noreply.github.com>
2018-03-31 01:50:34 -0400

    Add project files.

---
+++ RockPaperScissorsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RockPaperScissorsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
